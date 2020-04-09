bf12bdbcbd974aeea36c4a25a7c1d358821f5d8c
meocutee123 <49399040+meocutee123@users.noreply.github.com>
2020-04-09 10:40:34 +0700

    My first andoid app

---
+++ app/src/main/java/com/example/tuannghia_59131580_myfirstapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tuannghia_59131580_myfirstapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
